8670cd23af406b7b790d2de29caff11005dd1fb2
Ashwrai <dimmedframe@gmail.com>
2024-01-20 12:51:20 +0100

    Signup

--- src/test/java/features/signup.feature
+++ src/test/java/features/signup.feature
@@ -4,14 +4,6 @@ Feature: Sign up
 # Scenarios include registration with a valid email, handling repeated emails, successful registration,
 # and detecting and handling incomplete registrations.
 
-  Scenario: Valid Email
-
-    Given the user is in the sign up page
-    When the user enters name in the name input
-    And the user enters email in the email input
-    And the user clicks sign up button
-    Then the sign up form appears
-
   Scenario: Invalid Email
 
     Given the user is in the sign up page
@@ -19,6 +11,7 @@ Feature: Sign up
     And the user enters repeated email in the email input
     And the user clicks sign up button
     Then the invalid email message appears
+    And close
 
   Scenario: Successful Sign Up
 
@@ -32,13 +25,13 @@ Feature: Sign up
     And the user enters First name in the First name input
     And the user enters Last name in the Last name input
     And the user enters Address in the Address input
-    And the user selects Country
     And the user enters State in the State input
     And the user enters City in the City input
     And the user enters Zipcode in the Zipcode input
     And the user enters Mobile Number in the Mobile Number input
-    And the user clicks sign up button
+    And the user clicks create account button
     Then the user account is created
+    And close
 
   Scenario: Incomplete Sign Up
 
@@ -47,5 +40,6 @@ Feature: Sign up
     And the user enters email in the email input
     And the user clicks sign up button
     Then the sign up form appears
-    When the user clicks sign up button
-    Then the error message 'Fill out this field.' appears
+    When the user clicks create account button
+    Then the user stays at the same page
+    And close
--- src/test/java/steps/SignUpSteps.java
+++ src/test/java/steps/SignUpSteps.java
@@ -4,12 +4,22 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SignUpSteps {
@@ -40,8 +50,10 @@ public class SignUpSteps {
             driver.findElement(By.cssSelector("input[data-qa='signup-email'")).sendKeys(email);
 
             try {
-                FileWriter increaseNumber = new FileWriter("emailNumber.txt", false);
-                increaseNumber.write(++number);
+                number++;
+                BufferedWriter writer = new BufferedWriter(new FileWriter("emailNumber.txt", false));
+                writer.write(Integer.toString(number));
+                writer.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -50,74 +62,116 @@ public class SignUpSteps {
             ex.printStackTrace();
         }
 
-
     }
 
     @And("the user clicks sign up button")
     public void theUserClicksSignUpButton() {
+        driver.findElement(By.cssSelector("button[data-qa='signup-button'")).click();
     }
 
     @Then("the sign up form appears")
     public void theSignUpFormAppears() {
-        driver.close();
+        String expectedUrl = "https://automationexercise.com/signup";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
     }
 
     @And("the user enters repeated email in the email input")
     public void theUserEntersRepeatedEmailInTheEmailInput() {
+        String email = "jhon@gmail.com";
+        driver.findElement(By.cssSelector("input[data-qa='signup-email'")).sendKeys(email);
     }
 
     @Then("the invalid email message appears")
     public void theInvalidEmailMessageAppears() {
-        driver.close();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("p[style='color: red;']")));
+        WebElement errorMessage = driver.findElement(By.cssSelector("p[style='color: red;']"));
+        Assert.assertEquals(errorMessage.getText(), "Email Address already exist!");
     }
 
     @When("the user selects title")
     public void theUserSelectsTitle() {
+        driver.findElement(By.id("id_gender1")).click();
     }
 
     @And("the user enters password in the password input")
     public void theUserEntersPasswordInThePasswordInput() {
+        driver.findElement(By.id("password")).sendKeys("jhon");
     }
 
     @And("the user enters First name in the First name input")
     public void theUserEntersFirstNameInTheFirstNameInput() {
+        driver.findElement(By.id("first_name")).sendKeys("jhon");
     }
 
     @And("the user enters Last name in the Last name input")
     public void theUserEntersLastNameInTheLastNameInput() {
+        driver.findElement(By.id("last_name")).sendKeys("doe");
     }
 
     @And("the user enters Address in the Address input")
     public void theUserEntersAddressInTheAddressInput() {
+        driver.findElement(By.id("address1")).sendKeys("Wallstreet Avenue, 77");
     }
 
     @And("the user selects Country")
     public void theUserSelectsCountry() {
+        WebElement selector = driver.findElement(By.id("country"));
+
+        Select select = new Select(selector);
+
+        select.selectByVisibleText("United States");
+
     }
 
     @And("the user enters State in the State input")
     public void theUserEntersStateInTheStateInput() {
+        driver.findElement(By.id("state")).sendKeys("New York");
     }
 
     @And("the user enters City in the City input")
     public void theUserEntersCityInTheCityInput() {
+        driver.findElement(By.id("city")).sendKeys("New York");
     }
 
     @And("the user enters Zipcode in the Zipcode input")
     public void theUserEntersZipcodeInTheZipcodeInput() {
+        driver.findElement(By.id("zipcode")).sendKeys("01234");
     }
 
     @And("the user enters Mobile Number in the Mobile Number input")
     public void theUserEntersMobileNumberInTheMobileNumberInput() {
+        driver.findElement(By.id("mobile_number")).sendKeys("01234");
     }
 
     @Then("the user account is created")
     public void theUserAccountIsCreated() {
-        driver.close();
+        String expectedUrl = "https://automationexercise.com/account_created";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
     }
 
-    @Then("the error message 'Fill out this field.' appears")
-    public void theErrorMessageFillOutThisFieldAppears() {
+    @Then("the user stays at the same page")
+    public void theUserStaysAtTheSamePage() {
+        String expectedUrl = "https://automationexercise.com/signup";
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlToBe(expectedUrl));
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+    }
 
+    @And("the user clicks create account button")
+    public void theUserClicksCreateAccountButton() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,1395)");
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("button[data-qa='create-account'"));
+        button.click();
+    }
+    @Then("close")
+    public void close() {
+        driver.close();
     }
 }
